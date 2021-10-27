45bdcb0c9bcd222b7d7ae45ea1115a1249cdcfc7
Artsiom Lutskin <ALutskin@gomel.iba.by>
2021-10-27 19:28:38 +0300

    ReactJS - T02.02 - Understanding the Basic

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,30 @@
-import './Card.css';
+import "./Card.css";
 
 function Card() {
+  const checkboxHandler = (event) => {
+    const card = document.querySelector('.card');
+    const caption = document.querySelector('.caption');
+    const text = document.querySelector('.text');
+
+    if (event.target.checked) {
+      card.classList.add('darkCard');
+      caption.classList.add('darkCaption');
+      text.classList.add('darkText');
+    } else {
+      card.classList.remove('darkCard');
+      caption.classList.remove('darkCaption');
+      text.classList.remove('darkText');
+    }
+  };
+
   return (
     <div className="card">
       <h2 className="caption">Таблица цветов</h2>
-      <hr color="#3f3f3f" />
+
+      <input type="checkbox" id="one" onChange={checkboxHandler} />
+
+      <hr color="#3f3f3f" className="line" />
+
       <p className="text">
         Для сохранения единообразия страниц, цвета в МедиаВики нужно
         использовать обдуманно. Яркие цвета в статьях должны использоваться
