3e1584b906e97685e3eb0c0ef42ce3ea7ec29de1
bathord <alutskin@bk.ru>
2022-09-28 11:26:07 +0300

    Make Card component to work with Redux store
    Move most of Redux logic from CardList to Card and make Card to receive only id
    in props.

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,41 +1,59 @@
-import { useState, memo } from "react";
+import { useState, memo, useCallback } from "react";
 import PropTypes from "prop-types";
-
-import classes from "./Card.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { AiFillEdit } from "react-icons/ai";
 import { AiFillCheckCircle } from "react-icons/ai";
 import { AiFillCloseCircle } from "react-icons/ai";
+
+import classes from "./Card.module.css";
 import CardHeader from "../CardHeader/CardHeader";
 import CardBody from "../CardBody/CardBody";
-import { useNavigate } from "react-router-dom";
+import { rootActions } from "../../store/rootSlice";
 
 function Card(props) {
+  const id = props.id;
+  const {
+    caption,
+    text,
+    checked,
+  } = useSelector((store) => store.root.cardsData.find(cardData => cardData.id === id));
+  const readOnly = useSelector((store) => store.root.readOnly);
   const [editable, setEditable] = useState(false);
-  const [curCaptionValue, setCurCaptionValue] = useState(props.caption);
-  const [curTextValue, setCurTextValue] = useState(props.text);
+  const [curCaptionValue, setCurCaptionValue] = useState(caption);
+  const [curTextValue, setCurTextValue] = useState(text);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const updateCheckedStatus = useCallback((status) => {
+    dispatch(rootActions.updateCheckedStatus({ id, status }));
+  }, [dispatch, id]);
+
+  const updateContent = useCallback((newCaption, newText) => {
+    dispatch(rootActions.updateCardContent({ id, newCaption, newText }));
+  }, [dispatch, id]);
 
   const checkboxHandler = (event) => {
     if (event.target.checked) {
-      props.onUpdateCheckedStatus(true, props.id);
+      updateCheckedStatus(true);
     } else {
-      props.onUpdateCheckedStatus(false, props.id);
+      updateCheckedStatus(false);
     }
   };
 
   const editClickHandler = () => {
     setEditable(true);
-    props.onUpdateCheckedStatus(false, props.id);
+    updateCheckedStatus(false);
   };
 
   const saveChangesHandler = () => {
-    props.onUpdateContent(curCaptionValue, curTextValue, props.id);
+    updateContent(curCaptionValue, curTextValue);
     setEditable(false);
   };
 
   const cancelChangesHandler = () => {
-    setCurCaptionValue(props.caption);
-    setCurTextValue(props.text);
+    setCurCaptionValue(caption);
+    setCurTextValue(text);
     setEditable(false);
   };
 
@@ -49,22 +67,22 @@ function Card(props) {
 
   const cardDoubleClickHandler = () => {
     if (!editable) {
-      navigate(`/card/${props.id}`);
+      navigate(`/card/${id}`);
     }
   };
 
-  if (editable && props.readOnly) {
+  if (editable && readOnly) {
     cancelChangesHandler();
   }
 
   return (
     <div
-      className={`${classes.card} ${props.checked ? classes["dark-card"] : ""}`}
+      className={`${classes.card} ${checked ? classes["dark-card"] : ""}`}
       onDoubleClick={cardDoubleClickHandler}
     >
       <CardHeader
         caption={curCaptionValue}
-        checked={props.checked}
+        checked={checked}
         editable={editable}
         captionChangeHandler={captionChangeHandler}
       />
@@ -72,15 +90,15 @@ function Card(props) {
       <input
         type="checkbox"
         className={classes["select-checkbox"]}
-        checked={props.checked}
+        checked={checked}
         style={editable ? { display: "none" } : { display: "inline" }}
         onChange={checkboxHandler}
       />
 
       <AiFillEdit
         className={classes["edit-icon"]}
-        color={props.checked ? "#C0C0C0" : "#3f3f3f"}
-        visibility={editable || props.readOnly ? "hidden" : "visible"}
+        color={checked ? "#C0C0C0" : "#3f3f3f"}
+        visibility={editable || readOnly ? "hidden" : "visible"}
         onClick={editClickHandler}
       />
 
@@ -99,13 +117,13 @@ function Card(props) {
       />
 
       <hr
-        color={props.checked ? "#C0C0C0" : "#3f3f3f"}
+        color={checked ? "#C0C0C0" : "#3f3f3f"}
         className={classes.line}
       />
 
       <CardBody
         text={curTextValue}
-        checked={props.checked}
+        checked={checked}
         editable={editable}
         textChangeHandler={textChangeHandler}
       />
@@ -115,12 +133,6 @@ function Card(props) {
 
 Card.propTypes = {
   id: PropTypes.string,
-  caption: PropTypes.string,
-  text: PropTypes.string,
-  checked: PropTypes.bool,
-  readOnly: PropTypes.bool,
-  onUpdateCheckedStatus: PropTypes.func,
-  onUpdateContent: PropTypes.func,
 };
 
 export default memo(Card);
--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,24 +1,13 @@
+import { useSelector } from "react-redux";
+
 import withLoadingDelay from "../../UI/withLoadingDelay/withLoadingDelay";
 import Card from "../Card/Card";
 import classes from "./CardList.module.css";
-import { useSelector, useDispatch } from "react-redux";
-import { rootActions } from "../../store/rootSlice";
-import { useCallback } from "react";
 
 const CardWithDelay = withLoadingDelay(Card);
 
 const CardList = () => {
   const cardsData = useSelector((store) => store.root.cardsData);
-  const readOnly = useSelector((store) => store.root.readOnly);
-  const dispatch = useDispatch();
-
-  const updateCheckedStatusHandler = useCallback((status, id) => {
-    dispatch(rootActions.updateCheckedStatus({ id, status }));
-  }, [dispatch]);
-
-  const updateContentHandler = useCallback((newCaption, newText, id) => {
-    dispatch(rootActions.updateCardContent({ id, newCaption, newText }));
-  }, [dispatch]);
 
   return (
     <div className={classes["card-list"]}>
@@ -26,12 +15,6 @@ const CardList = () => {
         <CardWithDelay
           id={cardData.id}
           key={cardData.id}
-          caption={cardData.caption}
-          text={cardData.text}
-          checked={cardData.checked}
-          readOnly={readOnly}
-          onUpdateCheckedStatus={updateCheckedStatusHandler}
-          onUpdateContent={updateContentHandler}
         />
       ))}
     </div>
